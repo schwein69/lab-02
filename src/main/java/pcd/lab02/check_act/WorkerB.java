41dd553c9a8792c1ecbb435d372835ad62f5aba3
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-03-03 15:02:00 +0100

    fixed checkact to atomic action

--- src/main/java/pcd/lab02/check_act/WorkerB.java
+++ src/main/java/pcd/lab02/check_act/WorkerB.java
@@ -1,24 +1,27 @@
 package pcd.lab02.check_act;
 
 public class WorkerB extends Thread {
-	
-	private BoundedCounter counter;
-	private int ntimes;
-	
-	public WorkerB(BoundedCounter c, int ntimes){
-		counter = c;
-		this.ntimes = ntimes;
-	}
-	
-	public void run(){
-		try {
-			for (int i = 0; i < ntimes; i++){
-				if (counter.getValue() < 1){
-					counter.inc();
-				}
-			}
-		} catch (Exception ex){
-			ex.printStackTrace();
-		}
-	}
+
+    private final BoundedCounter counter;
+    private int ntimes;
+
+    public WorkerB(BoundedCounter c, int ntimes) {
+        counter = c;
+        this.ntimes = ntimes;
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < ntimes; i++) {
+                synchronized (counter) {//azione atomica, cioè eseguo due cose insieme come unica cosa e poi lascio.
+                    if (counter.getValue() < 1) {
+                        counter.inc();
+                    }
+                }
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
